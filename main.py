da9560c20044d9c378d3cb42443f01e5ee03e364
Ezio <37951898+ezio416@users.noreply.github.com>
2024-02-08 15:40:45 -0700

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+# c 2024-02-08
+# m 2024-02-08
+
+from json import dump, loads
+from time import sleep
+
+from requests import get
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
